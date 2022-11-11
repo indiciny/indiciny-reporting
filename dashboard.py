0543ee02be47ae9870c89ab69411c92ab4313523
indiciny <117272343+indiciny@users.noreply.github.com>
2022-11-11 08:39:13 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,28 @@
 import streamlit as st
+import mysql.connector
 
-with st.form("my_form"):
-  st.write("Please provide admin credentials:")
-  user_name = st.text_input('Name')
-  password = st.text_input('Password',type="password")
-  
-  submitted = st.form_submit_button("Submit")
+if 'authorized' not in st.session_state:
+    st.session_state.authorized = False
+    with st.form("my_form"):
+        st.write("Please provide admin credentials:")
+        user_name = st.text_input('Name')
+        password = st.text_input('Password', type="password")
 
-if submitted:
-  st.write("checking...")
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        if user_name == st.secrets.admin and password == st.secrets.password:
+            st.session_state.authorized = True
+
+
+if st.session_state.authorized:
+    btnFetch = st.button('Fetch data')
+    if btnFetch:
+        conn = mysql.connector.connect(**st.secrets["tradb"])
+        query = "SELECT * FROM `indiciny_transactions`;"
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            query_result = cursor.fetchall()
+            
+    if query_result is not None:
+        query_result
